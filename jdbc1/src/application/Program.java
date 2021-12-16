3b40ca7874921628ef0c6e215e5be092bbabca9e
SaimonFill <saimongabriel@hotmail.com>
2021-12-15 23:03:40 -0300

    fix: Corrige inserção de dados no banco.

--- jdbc1/src/application/Program.java
+++ jdbc1/src/application/Program.java
@@ -41,59 +41,77 @@ public class Program {
 		
 		// Faz a inserção de dados no banco de dados
 		
+		
+//		try {
+//			conn = DB.getConnection();
+//			
+//			ps = conn.prepareStatement(
+//					"INSERT INTO seller "
+//							+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+//							+ "VALUES "
+//							+ "(?, ?, ?, ?, ?)", //deixa em espera os valores a serem preenchidos 
+//							Statement.RETURN_GENERATED_KEYS
+//					);
+//			
+//			// Substitui esses valores nos "?" acima
+//			ps.setString(1, "Carl Puerple");
+//			ps.setString(2, "carl@gmail.com");
+//			ps.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+//			ps.setDouble(4, 2000.00);
+//			ps.setInt(5, 4);
+//			
+//			int rowsAffected =  ps.executeUpdate();
+//			
+//			if (rowsAffected > 0) {
+//				rs = ps.getGeneratedKeys();
+//				while (rs.next()) {
+//					int id = rs.getInt(1);
+//					System.out.println("Done! Id = " + id);
+//					System.out.println();
+//				}
+//			}
+//			else {
+//				System.out.println("No rows affected!");
+//			}
+//			
+//			rs = st.executeQuery("select * from seller");
+//			
+//			while (rs.next()) {
+//				System.out.println(
+//						rs.getInt("Id") 
+//							+ ", " + rs.getString("Name")
+//							+ ", " + rs.getString("Email")
+//							+ ", " + rs.getDate("BirthDate")
+//							+ ", " + rs.getDouble("BaseSalary")
+//							+ ", " + rs.getInt("DepartmentId")
+//						);
+//			}
+//			System.out.println();
+//		}
+		
+		// Atualiza dados no banco
+		
 		try {
-			conn = DB.getConnection();
-			
 			ps = conn.prepareStatement(
-					"INSERT INTO seller "
-							+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-							+ "VALUES "
-							+ "(?, ?, ?, ?, ?)", //deixa em espera os valores a serem preenchidos 
-							Statement.RETURN_GENERATED_KEYS
+					"UPDATE seller "
+					+ "SET BaseSalary = BaseSalary + ? "
+					+ "WHERE "
+					+ "(DepartmentId = ?)" 
 					);
 			
-			// Substitui esses valores nos "?" acima
-			ps.setString(1, "Carl Puerple");
-			ps.setString(2, "carl@gmail.com");
-			ps.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-			ps.setDouble(4, 2000.00);
-			ps.setInt(5, 4);
+			ps.setDouble(1, 200.00);
+			ps.setInt(2, 2);
 			
 			int rowsAffected =  ps.executeUpdate();
 			
-			if (rowsAffected > 0) {
-				rs = ps.getGeneratedKeys();
-				while (rs.next()) {
-					int id = rs.getInt(1);
-					System.out.println("Done! Id = " + id);
-					System.out.println();
-				}
-			}
-			else {
-				System.out.println("No rows affected!");
-			}
-			
-			rs = st.executeQuery("select * from seller");
-			
-			while (rs.next()) {
-				System.out.println(
-						rs.getInt("Id") 
-							+ ", " + rs.getString("Name")
-							+ ", " + rs.getString("Email")
-							+ ", " + rs.getDate("BirthDate")
-							+ ", " + rs.getDouble("BaseSalary")
-							+ ", " + rs.getInt("DepartmentId")
-						);
-			}
-			System.out.println();
-			
+			System.out.println("Done! Rows affected: " + rowsAffected);
 		}
 		catch (SQLException e) {
 			e.printStackTrace();
 		}
-		catch (ParseException e) {
-			e.printStackTrace();
-		}
+//		catch (ParseException e) {
+//			e.printStackTrace();
+//		}
 		finally {
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
